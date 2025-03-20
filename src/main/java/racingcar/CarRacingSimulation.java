5659637f009d218ea78b1bc52bfc5bced52ae9fb
asa9874 <pjkpjk1129@gmail.com>
2025-03-20 13:43:44 +0900

    Feat: CarRacingSimulation  getWinners 메소드구현

--- src/main/java/racingcar/CarRacingSimulation.java
+++ src/main/java/racingcar/CarRacingSimulation.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CarRacingSimulation {
@@ -19,5 +20,22 @@ public class CarRacingSimulation {
                 car.move();
             }
         }
+    }
+
+    public void getWinners() {
+        List<String> winnerCarNames = new ArrayList<>();
+        List<Car> sortedCars = this.cars.stream()
+                .sorted(Comparator.comparingInt(Car::getLocation).reversed())
+                .toList();
+        int maxLocation = sortedCars.get(0).getLocation();
+
+        for (Car car : sortedCars) {
+            if (car.getLocation() == maxLocation) {
+                winnerCarNames.add(car.getName());
+            }
+            else break;
+        }
+        
+        System.out.print("최종 우승자는 "+ String.join(", ", winnerCarNames) +" 입니다.");
     }
 }
