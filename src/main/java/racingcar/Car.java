002023c42ee0e65f83a9bd373b135da089268db4
asa9874 <pjkpjk1129@gmail.com>
2025-03-20 13:17:10 +0900

    Feat: Car 클래스 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,32 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+    private String name = "홍길동";
+    private int location = 0;
+
+    public Car(String name) {
+        if( name == null || name.length()>5 || name.length()==0 ){
+            throw new IllegalArgumentException("[ERROR] Car Name Error");
+        }
+        this.name = name;
+        this.location = 0;
+    }
+
+    // 무작위 숫자가 4이상일때 움직임
+    public void move(){
+        if(new Random().nextInt(10) >= 4){
+            location++;
+        }
+        System.out.println(name + " : " + "-".repeat(location));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+}
